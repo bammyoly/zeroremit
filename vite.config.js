22e5741856a461f1c8e53c2d0efe72a34bef8954
Bammyoly <bammy@gmail.com>
2026-06-23 11:23:12 +0100

    update changes

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,16 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import wasm              from 'vite-plugin-wasm';
 import topLevelAwait     from 'vite-plugin-top-level-await';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    wasm(),           // handles .wasm imports inside @zama-fhe/relayer-sdk/web
+    wasm(),
     topLevelAwait(), 
+    nodePolyfills()
   ],
 
   resolve: {
@@ -24,14 +26,13 @@ export default defineConfig({
       'chart.js',
       'react-chartjs-2',
     ],
-    // Exclude the relayer-sdk from pre-bundling — the alias handles it,
-    // and WASM packages must not be pre-bundled by Vite
     exclude: [
       '@zama-fhe/relayer-sdk',
     ],
   },
   build: {
-    target: 'esnext', // required for top-level await in production build
+    target: 'esnext',
+    sourcemap: true
   },
   server: {
     allowedHosts: true,
